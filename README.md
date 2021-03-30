05eed44415564ef1b08204635e41cd764025d6b9
Kingsley Chijioke Nweke <primekings.kc@gmail.com>
2021-03-31 00:31:17 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+My First Web App: A Replica of the Shala App
+# shala-app
---
+++ js/main.js
@@ -0,0 +1,44 @@
+
+$(document).ready(function(){
+     /* ------- NavBar Shrink -------- */
+    $(window).on("scroll",function(){
+        if($(this).scrollTop() > 90){
+            $(".navbar").addClass("navbar-shrink");
+        }
+        else{
+            $(".navbar").removeClass("navbar-shrink");
+        }
+    });
+    /* ------- Video popup -------- */   
+    const videoSrc = $("#player-1").attr("src");
+    $(".video-play-btn, .video-popup").on("click", function(){
+        if($(".video-popup").hasClass("open")){
+            $(".video-popup").removeClass("open");
+            $("#player-1").attr("src", "");
+        }
+        else{
+            $(".video-popup").addClass("open");
+            if($("#player-1").attr("src")==''){
+                $("#player-1").attr("src", videoSrc);
+            }
+        }
+    });
+    /* ------------- Features Carousel -------------*/
+    $('.features-carousel').owlCarousel({
+    loop:true,
+    margin:0,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+        },
+        600:{
+            items:2,
+        },
+        1000:{
+            items:3,
+        }
+    }
+    });
+
+});
